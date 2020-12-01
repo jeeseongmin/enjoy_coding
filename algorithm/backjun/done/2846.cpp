be6390769b2d44c7546d7a3dc659bcd4972c9e18
21500706 <21500706@handong.edu>
2020-12-02 02:41:33 +0900

    final 2846.cpp

--- algorithm/backjun/done/2846.cpp
+++ algorithm/backjun/done/2846.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main() {
-  int N, current=0, before=0, asc=0, desc=0, flag=0, max=0;
+  int N, current=0, before=0, asc=0, max=0;
   cin >> N;
   vector<int> num(N, 0);
   for(int i=0; i<N; i++) {
@@ -12,7 +12,13 @@ int main() {
   before = num[0];
   for(int i=1; i<N; i++) {
     current = num[i];
-    if(before >= current)
+    if(before >= current) {
+      max = (asc > max) ? asc : max;
+      asc=0;
+    }
+    else asc+= (current-before);
+    before = current;
+    max = (asc > max) ? asc : max;
   }
-
+  cout << max;
 }
