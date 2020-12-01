3fdae814c11a955de37fbdcf1b3a2060a12bbbe8
21500706 <21500706@handong.edu>
2020-12-02 02:23:43 +0900

    edit 2846.cpp

--- algorithm/backjun/2846.cpp
+++ algorithm/backjun/2846.cpp
@@ -3,5 +3,16 @@
 using namespace std;
 
 int main() {
-  
+  int N, current=0, before=0, asc=0, desc=0, flag=0, max=0;
+  cin >> N;
+  vector<int> num(N, 0);
+  for(int i=0; i<N; i++) {
+    cin >> num[i];
+  }
+  before = num[0];
+  for(int i=1; i<N; i++) {
+    current = num[i];
+    if(before >= current)
+  }
+
 }
